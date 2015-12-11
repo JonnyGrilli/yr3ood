552e7b7fdd1ba6a2c6c8d43b4eb25bab6c2ccce0
JonnyGrilli <jg00181@surrey.ac.uk>
2015-12-11 15:53:18 +0000

    Update commands.h

--- commands.h
+++ commands.h
@@ -1,20 +1,10 @@
-#pragma once
+#ifndef COMMAND_H
+#define COMMAND_H
 
-#include <iostream>
+#include "node.h"
+#include <istream>
 
 using namespace std;
-
-class Prog;
-
-class Node{
-private:
-		
-		
-public:
-		Node(){};
-		~Node(){};
-		virtual void run()=0;
-};
 
 class Command:public Node{
 protected:
@@ -26,42 +16,4 @@ public:
 		float getValue();
 		friend istream& operator>>(istream& in, Command& a);
 };
-
-
-class Forward: public Command{
-private:
-		
-public:
-		~Forward();
-		Forward(){};
-		void run();
-};
-
-class Rotate: public Command{
-private:
-		
-public:
-		~Rotate();
-		Rotate(){};
-		void run();
-};
-
-class Jump: public Command{
-private:
-		
-public:
-		~Jump();
-		Jump(){};
-		void run();
-};
-
-class Repeat: public Command{
-private:
-		
-public:
-		~Repeat();
-		Repeat(){};
-		Prog* program;
-		friend istream& operator>>(istream& in, Repeat& a);
-		void run();
-};
+#endif
